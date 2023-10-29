2c40eb847feb6bab4405cd06068dd7a08e06699d
Mark Karman <mkarman08@gmail.com>
2023-10-28 21:26:59 -0700

    added list items

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae molestias consequatur in cumque quos pariatur neque eum tempora doloribus dolore, rerum non beatae deserunt at sit praesentium aliquid alias accusantium.'
+    },
+    {
+        id: 2,
+        rating: 7,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae molestias consequatur in cumque quos pariatur neque eum tempora doloribus dolore, rerum non beatae deserunt at sit praesentium aliquid alias accusantium.'
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae molestias consequatur in cumque quos pariatur neque eum tempora doloribus dolore, rerum non beatae deserunt at sit praesentium aliquid alias accusantium.'
+    }
+]
+
+export default FeedbackData
