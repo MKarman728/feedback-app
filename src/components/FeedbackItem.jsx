12676eabb6ccfab9c2c6fd056df8161c211fd0a3
Mark Karman <mkarman08@gmail.com>
2023-10-30 19:35:43 -0700

    added prop state passing

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { useState } from 'react';
 import Card from './shared/Card';
 import PropTypes from 'prop-types'
+import { FaTimes } from 'react-icons/fa'
 
-const FeedbackItem = ({item}) => {
+const FeedbackItem = ({item, handleDelete}) => {
 
   return (
     <Card>
         <div className="num-display">{item.rating}</div>
-        <div className="text-display">
-            {item.text}
-        </div>
+        <button className="close">
+            <FaTimes onClick ={()=>handleDelete(item.id)} color='purple' handleDelete ={(id)=>console.log(id)}/>
+        </button>
+        <div className="text-display">{item.text}</div>
     </Card>
   )
 }
