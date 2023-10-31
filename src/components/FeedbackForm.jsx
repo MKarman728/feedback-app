e27c6fa30723bd212dfc4c1f0faf9bef076e7714
Mark Karman <mkarman08@gmail.com>
2023-10-31 14:09:43 -0700

    added button

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import Card from './shared/Card'
-import {useState} from 'react'
+import { useState } from 'react'
+import Button from './shared/Button'
 
 function FeedbackForm() {
-    const [text, setText] = useState('')
+  const [text, setText] = useState('')
 
-    const handleTextChange = (e) =>{
-        setText(e.target.value);
-    }
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  }
   return <Card>
     <form>
-        <h2>How would you rate your service with us?</h2>
-        <div className="input-group">
-            <input onChange={handleTextChange} type="text" placeholder='Write a review' value = {text}/>
-            <button type="submit">Send</button>
-        </div>
+      <h2>How would you rate your service with us?</h2>
+      <div className="input-group">
+        <input onChange={handleTextChange} type="text" placeholder='Write a review' value={text} />
+        <Button type="submit">Send</Button>
+      </div>
     </form>
   </Card>
 }
