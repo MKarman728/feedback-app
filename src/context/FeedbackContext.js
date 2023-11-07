887cd2197ac0b1724756e01a856e860dff305f4f
Mark Karman <mkarman08@gmail.com>
2023-11-07 15:13:29 -0800

    updated readme file

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -11,12 +11,12 @@ export const FeedbackProvider = ({children}) =>{
     },
     {
         id: 2,
-        text: 'Thisis feedback item 2',
+        text: 'This is feedback item 2',
         rating: 9
     },
     {
         id: 3,
-        text: 'Thisis feedback item 3',
+        text: 'This is feedback item 3',
         rating: 7
     }])
 
