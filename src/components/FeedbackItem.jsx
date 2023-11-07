849705dc7e0d37ea4e4f7b07d6e2f77db9df24ed
Mark Karman <mkarman08@gmail.com>
2023-11-06 16:58:47 -0800

    adding edit state capabilities

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -3,16 +3,19 @@ import { useState } from 'react';
 import Card from './shared/Card';
 import { useContext } from 'react';
 import PropTypes from 'prop-types'
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes, FaEdit } from 'react-icons/fa'
 import FeedbackContext from '../context/FeedbackContext';
 
 const FeedbackItem = ({item}) => {
-  const {deleteFeedback} = useContext(FeedbackContext);
+  const {deleteFeedback, editFeedback} = useContext(FeedbackContext);
   return (
     <Card>
         <div className="num-display">{item.rating}</div>
         <button className="close">
             <FaTimes onClick ={()=>deleteFeedback(item.id)} color='purple' handleDelete ={(id)=>console.log(id)}/>
+        </button>
+        <button onClick={() =>editFeedback(item)} className="edit">
+          <FaEdit color = 'purple'/>
         </button>
         <div className="text-display">{item.text}</div>
     </Card>
